825dc0b0e592be27cfb69cfde099e1a78cbd3715
Serpant1ne <gartsmankonstantin@gmail.com>
2024-08-08 19:36:45 +0200

    make server

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	serveMux := http.NewServeMux()
+	server := &http.Server{
+		Handler: serveMux,
+		Addr:    ":8080",
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
