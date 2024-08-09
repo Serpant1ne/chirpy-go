d2d5c88c85d0720de718965516a3de2994a973da
Serpant1ne <gartsmankonstantin@gmail.com>
2024-08-09 14:51:33 +0200

    bugfix

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,17 +11,52 @@ const (
 	PORT         = "8080"
 )
 
+type apiConfig struct {
+	fileServerHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileServerHits++
+	return next
+}
+
+func (cfg *apiConfig) getHits() int {
+	return cfg.fileServerHits
+}
+func (cfg *apiConfig) resetHits() {
+	cfg.fileServerHits = 0
+}
+
 func main() {
 	mux := http.NewServeMux()
-	//main handle
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(FILEPATHROOT))))
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(FILEPATHROOT)))
+	apiCfg := apiConfig{
+		fileServerHits: 0,
+	}
+	//main handler
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(handler))
 
-	//server health
+	//server health handler
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	//req number handler
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", apiCfg.getHits())))
+	})
+
+	//req number reset handler
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
 		w.Header().Set("charset", "utf-8")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Ok"))
+		apiCfg.resetHits()
+		w.Write([]byte("Reset succesful"))
 	})
 
 	//server setup
